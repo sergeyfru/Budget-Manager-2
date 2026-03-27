bbaea3de4d58c9335f60fbd8ae2e77179547a371
Sergey Fruman <sn.fruman@gmail.com>
2026-03-27 23:31:06 +0300

    [backend] req_login_schema

--- server/src/schemas/user_auth_schema.ts
+++ server/src/schemas/user_auth_schema.ts
@@ -24,6 +24,9 @@ export type UserInfoSchema = z.infer<typeof user_info_schema>;
 export const req_login_schema = z.object({
   email: z.email().toLowerCase(),
   password: z.string().min(6, "Password must be at least 6 characters")
+      .regex(/[a-z]/, "Password must contain a lowercase letter")
+      .regex(/[A-Z]/, "Password must contain an uppercase letter")
+      .regex(/[0-9]/, "Password must contain a number"),
 });
 
 export type ReqLoginSchema = z.infer<typeof req_login_schema>;
